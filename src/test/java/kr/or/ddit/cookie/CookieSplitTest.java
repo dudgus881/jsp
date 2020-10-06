d69446a465f1e2919382112658ba16cf31137fe7
yhyu <dudgus884@gmail.com>
2020-10-07 08:51:17 +0900

    "cookie  . client side - 과제 확인"

---
+++ src/test/java/kr/or/ddit/cookie/CookieSplitTest.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.cookie;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CookieSplitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
