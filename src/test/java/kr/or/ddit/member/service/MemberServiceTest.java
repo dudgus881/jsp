93b11e1f9b1ecc3585bba433dcf7343c29f6e4be
yhyu <dudgus884@gmail.com>
2020-10-19 09:19:57 +0900

    "실습  . select box 변경시 언어 변경"

--- src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
+++ src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
@@ -2,17 +2,22 @@ package kr.or.ddit.member.service;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import kr.or.ddit.common.model.PageVo;
 import kr.or.ddit.member.dao.MemberDao;
 import kr.or.ddit.member.model.MemberVo;
 
 public class MemberServiceTest {
-
+	private static final Logger logger = LoggerFactory.getLogger(MemberServiceTest.class);
+	
 	@Test
 	public void getMemberTest() {
 		/***Given***/
@@ -52,5 +57,79 @@ public class MemberServiceTest {
 		assertEquals(3, pages);
 
 	}
+	
+	@Test
+	public void localeListTest() {
 
+		Locale[] locales =  SimpleDateFormat.getAvailableLocales();
+		
+		for(Locale locale : locales) {
+			logger.debug(locale.toString());
+//			logger.debug("{}",locale);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
