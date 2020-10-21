ffa88b891d6e816a9701b5ec9823581280f3419d
yhyu <dudgus884@gmail.com>
2020-10-21 09:25:47 +0900

    "Listener  . SessionListener,  SessionAttributeListener 등록  . userMap.jsp  . ServletContextListener 등록"

---
+++ src/main/java/kr/or/ddit/logout/LogoutServlet.java
@@ -0,0 +1,35 @@
+package kr.or.ddit.logout;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/Logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LoggerFactory.getLogger(LogoutServlet.class);
+
+	private ServletRequest session;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		session.removeAttribute("userid");
+		request.getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+}
